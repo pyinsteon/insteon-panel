c9195562e3f51b8b48882ce09ba7ff76313f4aa5
Tom Harris <tom@harrisnj.net>
2021-11-30 20:08:32 +0000

    Update device data

--- src/insteon-devices-panel.ts
+++ src/insteon-devices-panel.ts
@@ -1,69 +1,77 @@
-// import { mdiPuzzle } from "@mdi/js";
-// import "@material/mwc-list/mwc-list-item";
+import { UnsubscribeFunc } from "home-assistant-js-websocket";
 import { html, LitElement, TemplateResult, PropertyValues } from "lit";
 import { customElement, property } from "lit/decorators";
 import memoizeOne from "memoize-one";
 import "./data-table/insteon-data-table";
 import "../homeassistant-frontend/src/components/ha-card";
 import "../homeassistant-frontend/src/layouts/hass-subpage";
-// import { PageNavigation } from "../homeassistant-frontend/src/layouts/hass-tabs-subpage";
-// import "../homeassistant-frontend/src/components/ha-button-menu";
 import { HomeAssistant, Route } from "../homeassistant-frontend/src/types";
 import {
-  fetchDeviceRegistry,
+  subscribeDeviceRegistry,
   DeviceRegistryEntry,
 } from "../homeassistant-frontend/src/data/device_registry";
-import {
-  EntityRegistryEntry,
-  fetchEntityRegistry,
-} from "../homeassistant-frontend/src/data/entity_registry";
 import { Insteon } from "./data/insteon";
 import { navigate } from "../homeassistant-frontend/src/common/navigate";
 import { HASSDomEvent } from "../homeassistant-frontend/src/common/dom/fire_event";
 import { RowClickedEvent } from "./data-table/insteon-data-table";
+import { AreaRegistryEntry, subscribeAreaRegistry } from "../homeassistant-frontend/src/data/area_registry";
 
-export interface DeviceEntityLookup {
-  [deviceId: string]: EntityRegistryEntry[];
-}
 
 @customElement("insteon-devices-panel")
 export class InsteonDevicesPanel extends LitElement {
   @property({ attribute: false }) public hass!: HomeAssistant;
 
-  @property() public insteon!: Insteon;
+  @property({ type: Object }) public insteon!: Insteon;
 
-  @property() public route!: Route;
+  @property({ type: Object }) public route!: Route;
 
   @property({ type: Boolean }) public narrow = false;
 
-  @property() private _devices: DeviceRegistryEntry[] = [];
+  @property( {type: Array }) private _devices: DeviceRegistryEntry[] = [];
 
-  @property() private _entities: EntityRegistryEntry[] = [];
+  private _areas: AreaRegistryEntry[] = [];
+
+  private _unsubs?: UnsubscribeFunc[];
 
   // @state() private _showDisabled = false;
 
-  protected firstUpdated(changedProps: PropertyValues) {
-    super.firstUpdated(changedProps);
-    this._getDevices();
+  public connectedCallback() {
+    super.connectedCallback();
+
+    if (!this.hass || !this.insteon) {
+      return;
+    }
+    if (!this._unsubs) {
+      this._getDevices();
+    }
   }
 
-  protected updated(changedProps: PropertyValues) {
-    super.updated(changedProps);
+  public disconnectedCallback() {
+    super.disconnectedCallback();
+    if (this._unsubs) {
+      while (this._unsubs.length) {
+        this._unsubs.pop()!();
+      }
+      this._unsubs = undefined;
+    }
   }
 
   private _getDevices() {
     if (!this.insteon || !this.hass) {
       return;
     }
-    fetchDeviceRegistry(this.hass.connection).then((devices) => {
-      fetchEntityRegistry(this.hass.connection).then((entities) => {
-        this._entities = entities;
-        this._devices = devices.filter(
+    
+    this._unsubs = [
+      subscribeAreaRegistry(this.hass.connection, (areas) => {
+        this._areas = areas;
+      }),
+      subscribeDeviceRegistry(this.hass.connection, (entries) => {
+        this._devices = entries.filter(
           (device) =>
             device.config_entries && device.config_entries[0] == this.insteon.config_entry.entry_id
-        );
-      });
-    });
+        );;
+      }),
+    ];
   }
 
   private _columns = {
@@ -91,36 +99,21 @@ export class InsteonDevicesPanel extends LitElement {
   };
 
   private _insteonDevices = memoizeOne((devices: DeviceRegistryEntry[]) => {
-    /*
-    if (!this._devices || !this._entities) {
-      return [];
-    }
-    */
-    const deviceLookup: { [deviceId: string]: DeviceRegistryEntry } = {};
-    for (const device of this._devices) {
-      deviceLookup[device.id] = device;
-    }
-    const deviceEntityLookup: DeviceEntityLookup = {};
-    for (const entity of this._entities) {
-      if (!entity.device_id) {
-        continue;
-      }
-      if (!(entity.device_id in deviceEntityLookup)) {
-        deviceEntityLookup[entity.device_id] = [];
-      }
-      deviceEntityLookup[entity.device_id].push(entity);
-    }
 
-    // eslint-disable-next-line no-console
-    console.info("Insteon GUID: " + this.insteon.config_entry.entry_id);
+    const areaLookup: { [areaId: string]: AreaRegistryEntry } = {};
+    for (const area of this._areas) {
+      areaLookup[area.area_id] = area;
+    }
 
     return devices.map((device) => {
       return {
         ...device,
         id: device.id,
-        name: device.name, // computeDeviceName(device, this.hass, deviceEntityLookup[device.id]),
-        model: device.model || "<unknown>",
-        guid: device.config_entries[0],
+        name: device.name_by_user || device.name,
+        address: device.name?.substr(device.name.length - 7, 8) || "",
+        description: device.name?.substr(0, device.name.length - 8)|| "",
+        model: device.model || "",
+        area: device.area_id ? areaLookup[device.area_id]: ""
       };
     });
   });
